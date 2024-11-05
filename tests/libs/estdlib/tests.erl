8a0e2a61a92df4c5efd694512a1ee5bc43ac1828
Tomasz Sobkiewicz <tomasz@sobkiewicz.swmansion.com>
2024-11-05 11:27:22 +0100

    Add elixir.erl

--- tests/libs/estdlib/tests.erl
+++ tests/libs/estdlib/tests.erl
@@ -62,6 +62,7 @@ get_tests(_OTPVersion) ->
         test_queue,
         test_timer,
         test_spawn,
+        test_elixir,
         test_supervisor,
         test_lists_keymember,
         test_lists_keyfind,
