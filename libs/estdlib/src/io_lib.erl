8f84df706d29ed184bc7f5d171815b01977ec78c
Tomasz Sobkiewicz <tomasz@sobkiewicz.swmansion.com>
2024-11-12 11:37:22 +0100

    Add elixir_parser

--- libs/estdlib/src/io_lib.erl
+++ libs/estdlib/src/io_lib.erl
@@ -27,7 +27,11 @@
 %%-----------------------------------------------------------------------------
 -module(io_lib).
 
--export([format/2, fwrite/2, latin1_char_list/1, write_atom/1, write_atom_as_latin1/1]).
+-export([format/2, fwrite/2, latin1_char_list/1, write_atom/1, write_atom_as_latin1/1, deep_char_list/1,write/1,write/2,write/3, write_binary/3,write_string/1, quote_atom/2, write_char/1]).
+-type chars() :: [char() | chars()].
+-type depth() :: -1 | non_neg_integer().
+-type chars_limit() :: integer().
+-type latin1_string() :: [unicode:latin1_char()].
 
 -spec fwrite(Format, Data) -> chars() when
     Format :: io:format(),
@@ -616,6 +620,300 @@ char_class(27) -> latin1_printable;
 char_class(H) when H >= 14 andalso H < 32 -> latin1_unprintable;
 char_class(H) when H < 256 -> latin1_printable;
 char_class(_H) -> unicode.
+
+-spec deep_char_list(Term) -> boolean() when
+      Term :: term().
+
+deep_char_list(Cs) ->
+    deep_char_list(Cs, []).
+
+deep_char_list([C|Cs], More) when is_list(C) ->
+    deep_char_list(C, [Cs|More]);
+deep_char_list([C|Cs], More)
+  when is_integer(C), C >= 0, C < 16#D800;
+       is_integer(C), C > 16#DFFF, C < 16#FFFE;
+       is_integer(C), C > 16#FFFF, C =< 16#10FFFF ->
+    deep_char_list(Cs, More);
+deep_char_list([], [Cs|More]) ->
+    deep_char_list(Cs, More);
+deep_char_list([], []) -> true;
+deep_char_list(_, _More) ->		%Everything else is false
+    false.
+
+
+    -spec write(Term) -> chars() when
+Term :: term().
+
+write(Term) ->
+write1(Term, -1, latin1, undefined).
+
+-spec write(term(), depth(), boolean()) -> chars().
+
+write(Term, D, true) ->
+io_lib_pretty:print(Term, 1, 80, D);
+write(Term, D, false) ->
+write(Term, D).
+
+-spec write(Term, Depth) -> chars() when
+Term :: term(),
+Depth :: depth();
+     (Term, Options) -> chars() when
+Term :: term(),
+Options :: [Option],
+Option :: {'chars_limit', CharsLimit}
+        | {'depth', Depth}
+        | {'encoding', 'latin1' | 'utf8' | 'unicode'},
+CharsLimit :: chars_limit(),
+Depth :: depth().
+
+write(Term, Options) when is_list(Options) ->
+Depth = get_option(depth, Options, -1),
+Encoding = get_option(encoding, Options, epp:default_encoding()),
+CharsLimit = get_option(chars_limit, Options, -1),
+MapsOrder = get_option(maps_order, Options, undefined),
+if
+  Depth =:= 0; CharsLimit =:= 0 ->
+      "...";
+  is_integer(CharsLimit), CharsLimit < 0, is_integer(Depth) ->
+      write1(Term, Depth, Encoding, MapsOrder);
+  is_integer(CharsLimit), CharsLimit > 0 ->
+      RecDefFun = fun(_, _) -> no end,
+      If = io_lib_pretty:intermediate
+           (Term, Depth, CharsLimit, RecDefFun, Encoding, _Str=false, MapsOrder),
+      io_lib_pretty:write(If)
+end;
+write(Term, Depth) ->
+write(Term, [{depth, Depth}, {encoding, latin1}]).
+
+write1(_Term, 0, _E, _O) -> "...";
+write1(Term, _D, _E, _O) when is_integer(Term) -> integer_to_list(Term);
+write1(Term, _D, _E, _O) when is_float(Term) -> io_lib_format:fwrite_g(Term);
+write1(Atom, _D, latin1, _O) when is_atom(Atom) -> write_atom_as_latin1(Atom);
+write1(Atom, _D, _E, _O) when is_atom(Atom) -> write_atom(Atom);
+write1(Term, _D, _E, _O) when is_port(Term) -> write_port(Term);
+write1(Term, _D, _E, _O) when is_pid(Term) -> pid_to_list(Term);
+write1(Term, _D, _E, _O) when is_reference(Term) -> write_ref(Term);
+write1(<<_/bitstring>>=Term, D, _E, _O) -> write_binary(Term, D);
+write1([], _D, _E, _O) -> "[]";
+write1({}, _D, _E, _O) -> "{}";
+write1([H|T], D, E, O) ->
+if
+D =:= 1 -> "[...]";
+true ->
+  [$[,[write1(H, D-1, E, O)|write_tail(T, D-1, E, O)],$]]
+end;
+write1(F, _D, _E, _O) when is_function(F) ->
+erlang:fun_to_list(F);
+write1(Term, D, E, O) when is_map(Term) ->
+write_map(Term, D, E, O);
+write1(T, D, E, O) when is_tuple(T) ->
+if
+D =:= 1 -> "{...}";
+true ->
+  [${,
+   [write1(element(1, T), D-1, E, O)|write_tuple(T, 2, D-1, E, O)],
+   $}]
+end.
+
+%% write_tail(List, Depth, Encoding)
+%%  Test the terminating case first as this looks better with depth.
+
+write_tail([], _D, _E, _O) -> "";
+write_tail(_, 1, _E, _O) -> [$| | "..."];
+write_tail([H|T], D, E, O) ->
+[$,,write1(H, D-1, E, O)|write_tail(T, D-1, E, O)];
+write_tail(Other, D, E, O) ->
+[$|,write1(Other, D-1, E, O)].
+
+write_tuple(T, I, _D, _E, _O) when I > tuple_size(T) -> "";
+write_tuple(_, _I, 1, _E, _O) -> [$, | "..."];
+write_tuple(T, I, D, E, O) ->
+[$,,write1(element(I, T), D-1, E, O)|write_tuple(T, I+1, D-1, E, O)].
+
+write_port(Port) ->
+erlang:port_to_list(Port).
+
+write_ref(Ref) ->
+erlang:ref_to_list(Ref).
+
+write_map(_, 1, _E, _O) -> "#{}";
+write_map(Map, D, E, O) when is_integer(D) ->
+I = maps:iterator(Map, O),
+case maps:next(I) of
+  {K, V, NextI} ->
+      D0 = D - 1,
+      W = write_map_assoc(K, V, D0, E, O),
+      [$#,${,[W | write_map_body(NextI, D0, D0, E, O)],$}];
+  none -> "#{}"
+end.
+
+write_map_body(_, 1, _D0, _E, _O) -> ",...";
+write_map_body(I, D, D0, E, O) ->
+case maps:next(I) of
+  {K, V, NextI} ->
+      W = write_map_assoc(K, V, D0, E, O),
+      [$,,W|write_map_body(NextI, D - 1, D0, E, O)];
+  none -> ""
+end.
+
+write_map_assoc(K, V, D, E, O) ->
+[write1(K, D, E, O)," => ",write1(V, D, E, O)].
+
+write_binary(B, D) when is_integer(D) ->
+{S, _} = write_binary(B, D, -1),
+S.
+
+write_binary(B, D, T) when is_integer(T) ->
+{S, Rest} = write_binary_body(B, D, tsub(T, 4), []),
+{[$<,$<,lists:reverse(S),$>,$>], Rest}.
+
+write_binary_body(<<>> = B, _D, _T, Acc) ->
+{Acc, B};
+write_binary_body(B, D, T, Acc) when D =:= 1; T =:= 0->
+{["..."|Acc], B};
+write_binary_body(<<X:8>>, _D, _T, Acc) ->
+{[integer_to_list(X)|Acc], <<>>};
+write_binary_body(<<X:8,Rest/bitstring>>, D, T, Acc) ->
+S = integer_to_list(X),
+write_binary_body(Rest, D-1, tsub(T, length(S) + 1), [$,,S|Acc]);
+write_binary_body(B, _D, _T, Acc) ->
+L = bit_size(B),
+<<X:L>> = B,
+{[integer_to_list(L),$:,integer_to_list(X)|Acc], <<>>}.
+
+%% Make sure T does not change sign.
+tsub(T, _) when T < 0 -> T;
+tsub(T, E) when T >= E -> T - E;
+tsub(_, _) -> 0.
+
+get_option(Key, TupleList, Default) ->
+    case lists:keyfind(Key, 1, TupleList) of
+	false -> Default;
+	{Key, Value} -> Value;
+	_ -> Default
+    end.
+
+    -spec write_atom(Atom) -> chars() when
+Atom :: atom().
+
+write_atom(Atom) ->
+write_possibly_quoted_atom(Atom, fun write_string/2).
+
+-spec write_atom_as_latin1(Atom) -> latin1_string() when
+Atom :: atom().
+
+write_atom_as_latin1(Atom) ->
+write_possibly_quoted_atom(Atom, fun write_string_as_latin1/2).
+
+write_possibly_quoted_atom(Atom, PFun) ->
+Chars = atom_to_list(Atom),
+case quote_atom(Atom, Chars) of
+true ->
+      PFun(Chars, $');   %'
+false ->
+  Chars
+end.
+
+-spec quote_atom(atom(), chars()) -> boolean().
+
+quote_atom(Atom, Cs0) ->
+    case erl_scan:reserved_word(Atom) of
+	true -> true;
+	false ->
+	    case Cs0 of
+		[C|Cs] when is_integer(C), C >= $a, C =< $z ->
+		    not name_chars(Cs);
+		[C|Cs] when is_integer(C), C >= $ß, C =< $ÿ, C =/= $÷ ->
+		    not name_chars(Cs);
+		[C|_] when is_integer(C) -> true;
+                [] -> true
+	    end
+    end.
+
+name_chars([C|Cs]) when is_integer(C) ->
+    case name_char(C) of
+	true -> name_chars(Cs);
+	false -> false
+    end;
+name_chars([]) -> true.
+
+name_char(C) when C >= $a, C =< $z -> true;
+name_char(C) when C >= $ß, C =< $ÿ, C =/= $÷ -> true;
+name_char(C) when C >= $A, C =< $Z -> true;
+name_char(C) when C >= $À, C =< $Þ, C =/= $× -> true;
+name_char(C) when C >= $0, C =< $9 -> true;
+name_char($_) -> true;
+name_char($@) -> true;
+name_char(_) -> false.
+
+
+%%% There are two functions to write Unicode strings:
+%%% - they both escape control characters < 160;
+%%% - write_string() never escapes characters >= 160;
+%%% - write_string_as_latin1() also escapes characters >= 255.
+
+%% write_string([Char]) -> [Char]
+%%  Generate the list of characters needed to print a string.
+
+-spec write_string(String) -> chars() when
+      String :: string().
+
+write_string(S) ->
+    write_string(S, $").   %"
+
+-spec write_string(string(), char()) -> chars().
+
+write_string(S, Q) ->
+    [Q|write_string1(unicode_as_unicode, S, Q)].
+
+
+-spec write_string_as_latin1(string(), char()) -> latin1_string().
+
+write_string_as_latin1(S, Q) ->
+    [Q|write_string1(unicode_as_latin1, S, Q)].
+
+write_string1(_,[], Q) ->
+    [Q];
+write_string1(Enc,[C|Cs], Q) when is_integer(C) ->
+    string_char(Enc,C, Q, write_string1(Enc,Cs, Q)).
+
+string_char(_,Q, Q, Tail) -> [$\\,Q|Tail];	%Must check these first!
+string_char(_,$\\, _, Tail) -> [$\\,$\\|Tail];
+string_char(_,C, _, Tail) when C >= $\s, C =< $~ ->
+    [C|Tail];
+string_char(latin1,C, _, Tail) when C >= $\240, C =< $\377 ->
+    [C|Tail];
+string_char(unicode_as_unicode,C, _, Tail) when C >= $\240 ->
+    [C|Tail];
+string_char(unicode_as_latin1,C, _, Tail) when C >= $\240, C =< $\377 ->
+    [C|Tail];
+string_char(unicode_as_latin1,C, _, Tail) when C >= $\377 ->
+    "\\x{"++erlang:integer_to_list(C, 16)++"}"++Tail;
+string_char(_,$\n, _, Tail) -> [$\\,$n|Tail];	%\n = LF
+string_char(_,$\r, _, Tail) -> [$\\,$r|Tail];	%\r = CR
+string_char(_,$\t, _, Tail) -> [$\\,$t|Tail];	%\t = TAB
+string_char(_,$\v, _, Tail) -> [$\\,$v|Tail];	%\v = VT
+string_char(_,$\b, _, Tail) -> [$\\,$b|Tail];	%\b = BS
+string_char(_,$\f, _, Tail) -> [$\\,$f|Tail];	%\f = FF
+string_char(_,$\e, _, Tail) -> [$\\,$e|Tail];	%\e = ESC
+string_char(_,$\d, _, Tail) -> [$\\,$d|Tail];	%\d = DEL
+string_char(_,C, _, Tail) when C < $\240->	%Other control characters.
+    C1 = (C bsr 6) + $0,
+    C2 = ((C bsr 3) band 7) + $0,
+    C3 = (C band 7) + $0,
+    [$\\,C1,C2,C3|Tail].
+
+
+%% write_char(Char) -> [char()].
+%%  Generate the list of characters needed to print a character constant.
+%%  Must special case SPACE, $\s, here.
+
+-spec write_char(Char) -> chars() when
+      Char :: char().
+
+write_char($\s) -> "$\\s";			%Must special case this.
+write_char(C) when is_integer(C), C >= $\000 ->
+    [$$|string_char(unicode_as_unicode, C, -1, [])].
 
 %% @private
 format_p_string([], Acc) ->
