bbe580fccc498ee36467cfcfcc6e9ed27de36080
Mateusz Front <mateusz.front@swmansion.com>
2024-11-13 15:13:08 +0100

    CR

--- examples/emscripten/eval_code.erl
+++ examples/emscripten/eval_code.erl
@@ -1,7 +1,7 @@
 %
 % This file is part of AtomVM.
 %
-% Copyright 2023 Paul Guyot <pguyot@kallisys.net>
+% Copyright 2023 Mateusz Front <mateusz.front@swmansion.com>
 %
 % Licensed under the Apache License, Version 2.0 (the "License");
 % you may not use this file except in compliance with the License.
