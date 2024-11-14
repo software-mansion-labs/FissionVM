33ebec0cbd24e027075928f2c643ffb5d84a5b14
Tomasz Sobkiewicz <tomasz.sobkiewicz@swmansion.com>
2024-11-14 13:26:45 +0100

    Added space

--- libs/estdlib/src/ets.erl
+++ libs/estdlib/src/ets.erl
@@ -158,7 +158,7 @@ update_counter(_Table, _Key, _Params, _Default) ->
 %%-----------------------------------------------------------------------------
 %% @param Table a reference to the ets table
 %% @param Key the key associated with the tuple that needs updating
-%% @param Changes tuple`{Position, Value}` indicating the position to update and new value.
+%% @param Changes tuple `{Position, Value}` indicating the position to update and new value.
 %%`Position` should be a positive integer referring to the 1-based index within the tuple.
 %% @returns true if the element under specific key was found, otherwise false.
 %% @doc Updates specific elements of a tuple in an ets table.
