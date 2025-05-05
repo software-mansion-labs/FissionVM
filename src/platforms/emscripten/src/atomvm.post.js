8516946e9e9673a3abd6c0ebc27732970f32a5ee
Jakub Gonet <jakub.gonet@swmansion.com>
2025-05-05 15:13:05 +0200

    Use only EXPORT_ES6 option
    Signed-off-by: Jakub Gonet <jakub.gonet@swmansion.com>

--- src/platforms/emscripten/src/atomvm.post.js
+++
@@ -1,4 +0,0 @@
-Module["getPromiseFromId"] = (promiseId) => promiseMap.get(promiseId).promise;
-Module["_call"] = __exp__call;
-Module["_cast"] = __exp__cast;
-Module["_next_remote_object_key"] = __exp__next_remote_object_key;
