0c7689c29c6f6c4d5ff07399b7f161e83089e978
Jakub Gonet <jakub.gonet@swmansion.com>
2025-09-20 13:38:45 +0300

    wasm: add TrackedObject resource
    Signed-off-by: Jakub Gonet <jakub.gonet@swmansion.com>

--- src/platforms/emscripten/src/lib/sys.c
+++ src/platforms/emscripten/src/lib/sys.c
@@ -58,15 +58,9 @@
 void sys_promise_resolve_int_and_destroy(em_promise_t promise, em_promise_result_t result, int value)
 {
     if (result == EM_PROMISE_FULFILL) {
-        EM_ASM({
-            promiseMap.get($0).resolve($1);
-        },
-            promise, value);
+        EM_ASM({ promiseMap.get($0).resolve($1); }, promise, value);
     } else {
-        EM_ASM({
-            promiseMap.get($0).reject($1);
-        },
-            promise, value);
+        EM_ASM({ promiseMap.get($0).reject($1); }, promise, value);
     }
     emscripten_promise_destroy(promise);
 }
@@ -81,15 +75,9 @@ void sys_promise_resolve_int_and_destroy(em_promise_t promise, em_promise_result
 void sys_promise_resolve_str_and_destroy(em_promise_t promise, em_promise_result_t result, int value)
 {
     if (result == EM_PROMISE_FULFILL) {
-        EM_ASM({
-            promiseMap.get($0).resolve(UTF8ToString($1));
-        },
-            promise, value);
+        EM_ASM({ promiseMap.get($0).resolve(UTF8ToString($1)); }, promise, value);
     } else {
-        EM_ASM({
-            promiseMap.get($0).reject(UTF8ToString($1));
-        },
-            promise, value);
+        EM_ASM({ promiseMap.get($0).reject(UTF8ToString($1)); }, promise, value);
     }
     emscripten_promise_destroy(promise);
 }
@@ -130,6 +118,19 @@ static void htmlevent_user_data_down(ErlNifEnv *caller_env, void *obj, ErlNifPid
     }
 }
 
+static void do_remove_tracked_object(atomic_size_t key)
+{
+    EM_ASM({ Module['onTrackedObjectDelete']($0); }, key);
+}
+
+static void tracked_object_dtor(ErlNifEnv *caller_env, void *obj)
+{
+    UNUSED(caller_env);
+
+    struct TrackedObjectResource *tracked_object_rsrc = (struct TrackedObjectResource *) obj;
+    emscripten_dispatch_to_thread(emscripten_main_runtime_thread_id(), EM_FUNC_SIG_VI, do_remove_tracked_object, NULL, tracked_object_rsrc->key);
+}
+
 static const ErlNifResourceTypeInit promise_resource_type_init = {
     .members = 1,
     .dtor = promise_dtor,
@@ -140,6 +141,11 @@ static const ErlNifResourceTypeInit htmlevent_user_data_resource_type_init = {
     .dtor = htmlevent_user_data_dtor,
     .stop = NULL,
     .down = htmlevent_user_data_down,
+};
+
+static const ErlNifResourceTypeInit tracked_object_resource_type_init = {
+    .members = 1,
+    .dtor = tracked_object_dtor
 };
 
 void sys_init_platform(GlobalContext *glb)
@@ -160,19 +166,28 @@ void sys_init_platform(GlobalContext *glb)
     list_init(&platform->messages);
     ErlNifEnv env;
     erl_nif_env_partial_init_from_globalcontext(&env, glb);
+
     platform->promise_resource_type = enif_init_resource_type(&env, "promise", &promise_resource_type_init, ERL_NIF_RT_CREATE, NULL);
     if (IS_NULL_PTR(platform->promise_resource_type)) {
         fprintf(stderr, "Cannot initialize promise_resource_type");
         AVM_ABORT();
     }
+
     platform->htmlevent_user_data_resource_type = enif_init_resource_type(&env, "htmlevent_user_data", &htmlevent_user_data_resource_type_init, ERL_NIF_RT_CREATE, NULL);
     if (IS_NULL_PTR(platform->htmlevent_user_data_resource_type)) {
         fprintf(stderr, "Cannot initialize htmlevent_user_data_resource_type");
         AVM_ABORT();
     }
+
     platform->websocket_resource_type = enif_init_resource_type(&env, "websocket", &websocket_resource_type_init, ERL_NIF_RT_CREATE, NULL);
     if (IS_NULL_PTR(platform->websocket_resource_type)) {
         fprintf(stderr, "Cannot initialize websocket_resource_type");
+        AVM_ABORT();
+    }
+
+    platform->tracked_object_resource_type = enif_init_resource_type(&env, "tracked_object", &tracked_object_resource_type_init, ERL_NIF_RT_CREATE, NULL);
+    if (IS_NULL_PTR(platform->tracked_object_resource_type)) {
+        fprintf(stderr, "Cannot initialize tracked_object resource type");
         AVM_ABORT();
     }
 
