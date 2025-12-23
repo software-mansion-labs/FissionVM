6684995eb137549e1076c1d70ed9d2f14376d546
Mateusz Furga <mateusz.furga@swmansion.com>
2025-12-23 14:21:29 +0100

    Add docs to multimap functions

--- src/libAtomVM/popcorn/popcorn_ets_multimap.h
+++ src/libAtomVM/popcorn/popcorn_ets_multimap.h
@@ -21,8 +21,8 @@
 #ifndef _POPCORN_ETS_MULTIMAP_H_
 #define _POPCORN_ETS_MULTIMAP_H_
 
-#include "globalcontext.h"
-#include "term.h"
+#include "../globalcontext.h"
+#include "../term.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -32,9 +32,9 @@ extern "C" {
 
 typedef enum EtsMultimapType
 {
-    EtsMultimapTypeOne, /* Only one value per key */
-    EtsMultimapTypeSet, /* Only unique values per key */
-    EtsMultimapTypeList /* Allow duplicate values per key */
+    EtsMultimapTypeOne, // Only one value per key
+    EtsMultimapTypeSet, // Only unique values per key
+    EtsMultimapTypeList // Allow duplicate values per key
 } EtsMultimapType;
 
 typedef enum EtsMultimapStatus
@@ -44,12 +44,12 @@ typedef enum EtsMultimapStatus
     EtsMultimapAllocationError
 } EtsMultimapStatus;
 
-struct EtsMultimap
+typedef struct EtsMultimap
 {
     EtsMultimapType type;
     size_t index;
     struct EtsMultimapNode *buckets[NUM_BUCKETS];
-};
+} EtsMultimap;
 
 struct EtsMultimapNode
 {
@@ -64,25 +64,71 @@ struct EtsMultimapEntry
     Heap *heap;
 };
 
-struct EtsMultimap *ets_multimap_new(EtsMultimapType type, size_t index);
+/**
+ * @brief Create a new multimap.
+ *
+ * @param type the multimap type
+ * @param index the index of the tuple element to use as key
+ * @return the created multimap, or NULL on error
+ */
+EtsMultimap *ets_multimap_new(EtsMultimapType type, size_t index);
 
-void ets_multimap_delete(struct EtsMultimap *multimap, GlobalContext *global);
+/**
+ * @brief Delete the multimap and all its contents.
+ *
+ * @param multimap the multimap
+ * @param global the global context
+ */
+void ets_multimap_delete(EtsMultimap *multimap, GlobalContext *global);
 
+/**
+ * @brief Insert one or more tuples into the multimap.
+ *
+ * @param multimap the multimap
+ * @param tuples the tuples to insert
+ * @param count the number of tuples to insert
+ * @return EtsMultimapOk on success, otherwise an error status
+ *
+ * @note Terms passed to this function will be copied to the ETS heap.
+ */
 EtsMultimapStatus ets_multimap_insert(
-    struct EtsMultimap *multimap,
+    EtsMultimap *multimap,
     term *tuples,
     size_t count,
     GlobalContext *global);
 
+/**
+ * @brief Lookup tuples by key.
+ *
+ * @param multimap the multimap
+ * @param key the key to lookup
+ * @param[out] tuples the found tuples (or NULL to only get the count)
+ * @param[out] count the number of found tuples
+ * @param global the global context
+ * @return EtsMultimapOk on success, otherwise an error status
+ *
+ * @note Terms returned by this function come from the ETS heap and should be copied
+ *       to the process heap if needed.
+ * @warning The caller is responsible for freeing the memory pointed to by `tuples`
+ *          using `free()`.
+ */
 EtsMultimapStatus ets_multimap_lookup(
-    struct EtsMultimap *multimap,
+    EtsMultimap *multimap,
     term key,
     term **tuples,
     size_t *count,
     GlobalContext *global);
 
+/**
+ * @brief Remove all tuples with the given key.
+ *
+ * @param multimap the multimap
+ * @param key the key to lookup
+ * @param global the global context
+ * @return EtsMultimapOk on success, otherwise an error status
+ */
 EtsMultimapStatus ets_multimap_remove(
-    struct EtsMultimap *multimap,
+    EtsMultimap *multimap,
     term key,
     GlobalContext *global);
 
